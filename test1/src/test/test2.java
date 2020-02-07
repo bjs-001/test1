ba2d97c450dc5cd24e20b7749a37895905689875
bjs <84179557@qq.com>
2020-02-07 16:02:21 +0800

    添加了cgb1908

--- test1/src/test/test2.java
+++ test1/src/test/test2.java
@@ -2,7 +2,9 @@ package test;
 
 public class test2 {
 	public static void main(String[] args) {
+		System.out.println("cgb1908");
 		System.out.println("cgb1909");
 		System.out.println("cgb1910");
+		
 	}
 }
