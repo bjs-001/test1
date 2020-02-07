67c71120d2d31695db66ff518a184291883fccf7
bjs <84179557@qq.com>
2020-02-07 15:56:28 +0800

    新建分支

--- test1/src/test/test2.java
+++ test1/src/test/test2.java
@@ -3,5 +3,7 @@ package test;
 public class test2 {
 	public static void main(String[] args) {
 		System.out.println("cgb1909");
+		System.out.println("cgb1910");
+		System.out.println("cgb1908");
 	}
 }
